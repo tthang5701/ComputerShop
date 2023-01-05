4f3412ae8bff2bd758d40dc72fa9f7041e3358c9
tranthuhang <tranhang5701@gmail.com>
2023-01-05 22:02:56 +0700

    delete category v2

--- mvc196/PHPAdmin/deleteCategory.php
+++ mvc196/PHPAdmin/deleteCategory.php
@@ -5,12 +5,12 @@ if(isset($_POST['deletedata']))
 {
     $id = $_POST['delete_id'];
 
-	$query = "DELETE FROM categories WHERE id='$id' ";
+	$query = "UPDATE categories SET status = 0 WHERE id='$id'";
     $query_run = mysqli_query($connection, $query);
  
 	if($query_run)
 	{
-		$_SESSION['status'] = "Xóa thành công";
+		$_SESSION['status'] = "Ẩn thành công";
 		$_SESSION['status_code'] = "success";
 		header('Location: category.php'); 
 	}
